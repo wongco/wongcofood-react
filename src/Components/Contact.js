4496f2dfb37b84f6e0dd6c6419741d58cdfe3b89
wongco <ginson@gmail.com>
2019-01-31 21:17:19 -0800

    update contact bar

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -1,63 +1,60 @@
 import React, { Component } from 'react';
 import styled from 'styled-components';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import {
-  faCoffee,
-  faEnvelope,
-  faPenSquare
-} from '@fortawesome/free-solid-svg-icons';
+import { faCoffee, faPenSquare } from '@fortawesome/free-solid-svg-icons';
+import { faEnvelope } from '@fortawesome/free-regular-svg-icons';
 import { faLinkedin, faGithub } from '@fortawesome/free-brands-svg-icons';
 
 const StyledContactContainer = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: center;
+  align-items: center;
   background-color: rgb(205, 180, 140);
+  height: 70px;
+  box-sizing: border-box;
+`;
+
+const StyledButton = styled.button`
+  margin: 0 15px;
+  padding: 5px 10px;
+  border-radius: 5px;
+  border: none;
+  background-color: #343a40;
+  color: white;
+
+  &:hover {
+    background-color: gray;
+  }
+`;
+
+const StyledFontAwesomeIcon = styled(FontAwesomeIcon)`
+  margin: 0 15px;
+  color: #343a40;
+  &:hover {
+    color: gray;
+  }
 `;
 
 const Contact = () => {
   return (
     <StyledContactContainer>
-      <FontAwesomeIcon icon={faCoffee} size={'2x'} />
-      <FontAwesomeIcon icon={faEnvelope} size={'2x'} />
-      <FontAwesomeIcon icon={faPenSquare} size={'2x'} />
-      <FontAwesomeIcon icon={faLinkedin} size={'2x'} />
-      <FontAwesomeIcon icon={faGithub} size={'2x'} />
-      {/* <div
-        class="container-fluid contact text-center contact__background-color py-3"
-        id="contact"
-      >
-        <div class="row justify-content-center d-flex align-items-center">
-          <div>
-            <a href="mailto:gin@wongcofood.com">
-              <i class="px-3 far fa-envelope fa-2x" />
-            </a>
-          </div>
-          <div>
-            <a href="https://wongco.github.io/wongcoblog/">
-              <i class="px-3 fas fa-pen-square fa-2x" />
-            </a>
-          </div>
-          <div>
-            <a href="https://github.com/wongco">
-              <i class="px-3 fab fa-github fa-2x" />
-            </a>
-          </div>
-          <div>
-            <a href="https://www.linkedin.com/in/ginson-wong-92610358">
-              <i class="px-3 fab fa-linkedin fa-2x" />
-            </a>
-          </div>
-          <div>
-            <a
-              href="resume.pdf"
-              class="ml-2 py-auto btn text-white button--contact"
-            >
-              Resume
-            </a>
-          </div>
-        </div>
-      </div> */}
+      <StyledFontAwesomeIcon icon={faCoffee} size={'2x'} />
+      <a href="mailto:gin@wongcofood.com">
+        <StyledFontAwesomeIcon icon={faEnvelope} size={'2x'} />
+      </a>
+      <a href="https://wongco.github.io/wongcoblog/">
+        <StyledFontAwesomeIcon icon={faPenSquare} size={'2x'} />
+      </a>
+      <a href="https://github.com/wongco">
+        <StyledFontAwesomeIcon icon={faGithub} size={'2x'} />
+      </a>
+      <a href="https://www.linkedin.com/in/ginson">
+        <StyledFontAwesomeIcon icon={faLinkedin} size={'2x'} />
+      </a>
+      <a href="resume.pdf">
+        <StyledButton type="button">Resume</StyledButton>
+      </a>
     </StyledContactContainer>
   );
 };
