3f2f7d7a2736d19a70ef4f213548b7654aed7116
wongco <ginson@gmail.com>
2019-02-09 23:47:07 -0800

    Update about section to contain info text

--- src/Components/Organisms/About.js
+++ src/Components/Organisms/About.js
@@ -2,7 +2,14 @@ import React from 'react';
 //import styled from 'styled-components';
 
 const About = () => {
-  return <section id="about">Things About Meeee - Placeholder</section>;
+  return (
+    <section id="about">
+      Having crafted solutions as a Systems Admin/Engineer, I firmly believe in
+      the ability of technology to bring people together and address both
+      societal and business challenges. I now take that same passion for solving
+      problems to full stack web development.
+    </section>
+  );
 };
 
 export default About;
