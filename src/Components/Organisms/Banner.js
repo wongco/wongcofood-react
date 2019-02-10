4a3d010a5446d2f936556f0fbb03896f7c6279f5
wongco <ginson@gmail.com>
2019-02-09 23:46:39 -0800

    Update banner to use self sizing image

--- src/Components/Organisms/Banner.js
+++ src/Components/Organisms/Banner.js
@@ -2,43 +2,45 @@ import React, { Component } from 'react';
 import bannerImage from '../../pics/baybridge_banner.png';
 import styled from 'styled-components';
 
-const StyledBanner = styled.section`
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-  font-weight: 600px;
-  color: white;
+// const StyledBanner = styled.section`
+//   display: flex;
+//   justify-content: space-around;
+//   align-items: center;
+//   font-weight: 600px;
+//   color: white;
+//   height: 100%;
+//   width: 100%;
+//   object-fit: contain;
+//   background-image: url(${bannerImage});
+//   background-repeat: no-repeat;
+//   background-position: center;
+//   background-color: black;
+//   object-fit: contain;
+//   height: 607px;
+// `;
+
+// const BannerQuoteContainer = styled.div`
+//   font-size: 150%;
+//   font-weight: 400;
+//   color: black;
+//   background-color: rgba(255, 255, 255, 0.65);
+//   padding: 30px 50px;
+//   border-radius: 10px;
+//   min-width: 200px;
+//   max-width: 800px;
+// `;
+
+const StyledImg = styled.img`
+  height: 100%;
   width: 100%;
-  background-image: url(${bannerImage});
-  background-repeat: no-repeat;
-  background-position: center;
-  background-color: black;
-  /* object-fit: contain; */
-  height: 607px;
+  object-fit: contain;
 `;
-
-const BannerQuoteContainer = styled.div`
-  font-size: 150%;
-  font-weight: 400;
-  color: black;
-  background-color: rgba(255, 255, 255, 0.65);
-  padding: 30px 50px;
-  border-radius: 10px;
-  min-width: 200px;
-  max-width: 800px;
-`;
-
 class Banner extends Component {
   render() {
     return (
-      <StyledBanner id="home">
-        <BannerQuoteContainer>
-          Having crafted solutions as a Systems Admin/Engineer, I firmly believe
-          in the ability of technology to bring people together and address both
-          societal and business challenges. I now take that same passion for
-          solving problems to full stack web development.
-        </BannerQuoteContainer>
-      </StyledBanner>
+      <div id="home">
+        <StyledImg src={bannerImage} alt="banner-picture" />
+      </div>
     );
   }
 }
